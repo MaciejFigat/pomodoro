11242b7122959682460dfd6fb63a0757e02dad90
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-01-15 17:29:42 +0100

    deployment ready ver1.0

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,21 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API is running')
-  next()
-})
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+    next()
+  })
+}
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
