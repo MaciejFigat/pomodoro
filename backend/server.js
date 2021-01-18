6543c6a32e44d2e76522645dbbf1ef4a1a184fab
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-01-18 14:07:28 +0100

    deployment with heroku-postbuild v2.3 no heroku postbuild

--- backend/server.js
+++ backend/server.js
@@ -19,16 +19,11 @@ const app = express()
 app.use(express.json())
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
-  app.get('/', (req, res) => {
-    res.send('API is running ')
-    next()
-  })
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  // app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-  // app.get('*', (req, res) =>
-  //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  // )
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
 } else {
   app.get('/', (req, res) => {
     res.send('API is running')
