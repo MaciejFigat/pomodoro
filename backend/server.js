1715a00d4421db63c5c9aa5c7174d29042d1cb8d
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-01-18 18:47:01 +0100

    server troubles

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build'))
   )
 
   // app.use(express.static(path.join(__dirname, '/frontend/build')))
