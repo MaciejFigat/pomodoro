c58ccc22fb7861ae6049ac74174a21e0a3fa1d62
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-01-18 18:36:31 +0100

    changes in the server

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,9 @@ app.use(express.json())
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
-  app.get('/', (req, res) => {
-    res.send('API is running')
-    next()
-  })
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
 
   // app.use(express.static(path.join(__dirname, '/frontend/build')))
 
