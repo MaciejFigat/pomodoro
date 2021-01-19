19f07940531f57caed51c2e9049a32e063022605
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-01-19 10:31:19 +0100

    server changes

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,11 @@ const app = express()
 app.use(express.json())
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build'))
-  )
+  app.use(express.static('frontend/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
 
   // app.use(express.static(path.join(__dirname, '/frontend/build')))
 
