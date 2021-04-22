773e3cf2d82b1cc615b2bb588484de160c2b0b8b
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-04-22 19:18:40 +0200

    changed dirname in the server for service worker v16

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  app.get('./service-worker.js', (req, res) => {
+  app.get('service-worker.js', (req, res) => {
     res.sendFile(
       path.resolve(__dirname, 'frontend', 'src', 'service-worker.js')
     )
